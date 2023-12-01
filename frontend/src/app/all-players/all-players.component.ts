b4894b9196ff357ca62b1360ecf84c1bff71ae0f
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-12-01 12:12:53 +0100

    all players with cards

--- frontend/src/app/all-players/all-players.component.ts
+++ frontend/src/app/all-players/all-players.component.ts
@@ -5,28 +5,43 @@ import {ModalController} from '@ionic/angular';
 import {ActivatedRoute, Router} from '@angular/router';
 import {HttpClient} from '@angular/common/http';
 import {firstValueFrom} from 'rxjs';
-import {environment} from "../../environments/environment";
+import {CreatePlayerComponent} from '../create-player/create-player.component';
+import {UpdatePlayerComponent} from '../update-player/update-player.component';
+
 
 @Component({
   selector: 'app-all-players',
   template: `
 
+    <ion-toolbar>
+
+      <ion-title>Players</ion-title>
+
+    </ion-toolbar>
+
     <ion-content class="ion-padding" fullscreen="true">
-      <ion-searchbar placeholder="Search player.."></ion-searchbar>
-      <ion-card *ngFor="let player of dataService.players">
-        <ion-card-header>
-          <ion-card-subtitle>PLAYER</ion-card-subtitle>
-          <ion-card-title>{{player.fullName}}</ion-card-title>
-        </ion-card-header>
+      <div class="container">
+        <ion-searchbar animated="true" placeholder="Search players" debounce="100"
+                       (ionInput)="handleInput($event)"></ion-searchbar>
+        <ion-button (click)="openCreatePLayer()">Create</ion-button>
+      </div>
+      <div class="container">
+        <ion-card *ngFor="let player of dataService.players">
+          <ion-card-header>
+            <ion-card-subtitle>PLAYER</ion-card-subtitle>
+            <ion-card-title>{{player.fullName}}</ion-card-title>
+          </ion-card-header>
 
-        <ion-button fill="clear">Update</ion-button>
+          <ion-button fill="clear" (click)="openUpdatePlayer(player.playerId)">Update</ion-button>
 
-      </ion-card>
+        </ion-card>
+      </div>
+
     </ion-content>`,
   styleUrls: ['./all-players.component.scss'],
 })
 export class AllPlayersComponent {
-player:Player | undefined;
+  player: Player | undefined;
 
   constructor(public modalController: ModalController,
               public route: ActivatedRoute,
@@ -35,19 +50,41 @@ player:Player | undefined;
               public http: HttpClient) {
     this.getAllPlayers();
   }
-  resultsPerPage: number = 6;
-  currentPage: number = 1;
+
 
   async getAllPlayers() {
 
     const QueryParams = await firstValueFrom(this.route.queryParams);
-    const page = QueryParams['page'] ?? 1
-    const resultsPerPage = QueryParams['resultsPerPage'] ?? 2;
-    this.currentPage = Number.parseInt(page) ?? 1;
-    this.resultsPerPage = Number.parseInt(resultsPerPage) ?? 2;
-    this.dataService.players = await firstValueFrom<Player[]>(this.http.get<Player[]>(
-      environment.baseUrl + "/players?page=" + this.currentPage + "&resultsPerPage=" + this.resultsPerPage));
+    const query = QueryParams['searchTerm'] ?? '';
+    const call = this.http.get<Player[]>(`api/players/search?SearchTerm=${query}`);
+    this.dataService.players = await firstValueFrom<Player[]>(call);
 
   }
 
+
+  async handleInput($event: any) {
+    const query = $event.target.value;
+    const call = this.http.get<Player[]>(`api/players/search?SearchTerm=${query}`);
+    this.dataService.players = await firstValueFrom<Player[]>(call);
+  }
+
+  async openCreatePLayer() {
+    const modal = await this.modalController.create({
+      component: CreatePlayerComponent
+    });
+    modal.present();
+  }
+
+
+  async openUpdatePlayer(playerId: number | undefined) {
+    if (playerId !== undefined) {
+      const modal = await this.modalController.create({
+        component: UpdatePlayerComponent,
+        componentProps: {
+          playerId: playerId
+        }
+      });
+      modal.present();
+    }
+  }
 }
