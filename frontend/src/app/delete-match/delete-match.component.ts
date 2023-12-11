5806c30c1be873ae1bf1d117a33b37fc23050722
AlexKurdelska <alex.kurdelska@gmail.com>
2023-12-11 17:10:31 +0100

    Confirmation modal without CSS

--- frontend/src/app/delete-match/delete-match.component.ts
+++ frontend/src/app/delete-match/delete-match.component.ts
@@ -1,5 +1,9 @@
 import {Component} from "@angular/core";
 import {DataService} from "../data.service";
+import {firstValueFrom} from "rxjs";
+import {HttpClient, HttpErrorResponse} from "@angular/common/http";
+import {ToastController} from "@ionic/angular";
+import {Router} from "@angular/router";
 
 
 @Component({
@@ -8,6 +12,40 @@ import {DataService} from "../data.service";
 })
 
 export class DeleteMatchComponent {
-constructor(public dataService: DataService) {
-}
+  constructor(public http: HttpClient, public dataService: DataService, public toastController: ToastController, public router: Router) {
+  }
+
+  async deleteMatch(matchId: number | undefined) {
+    try {
+      await firstValueFrom(this.http.delete('/api/matches/' + matchId))
+      this.dataService.matchesWithPlayers=this.dataService.matchesWithPlayers.filter(a => a.id!=matchId)
+      this.router.navigate(['']);
+
+      const toast = await this.toastController.create({
+        message: 'Match was deleted',
+        duration: 1200,
+        color: 'success'
+      })
+      toast.present();
+    } catch (error: any) {
+
+      let errorMessage = 'Error';
+
+      if (error instanceof HttpErrorResponse) {
+
+        errorMessage = error.error?.message || 'Server error';
+      } else if (error.error instanceof ErrorEvent) {
+
+        errorMessage = error.error.message;
+      }
+
+      const toast = await this.toastController.create({
+        color: 'danger',
+        duration: 2000,
+        message: errorMessage
+      });
+
+      toast.present();
+    }
+  }
 }
