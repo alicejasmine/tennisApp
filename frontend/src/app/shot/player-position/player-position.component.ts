f0c547d4128fd46efe48cc3af561755d53309377
AlexKurdelska <alex.kurdelska@gmail.com>
2023-12-14 13:45:38 +0100

    Player postition on court as svgs

--- frontend/src/app/shot/player-position/player-position.component.ts
+++ frontend/src/app/shot/player-position/player-position.component.ts
@@ -13,47 +13,54 @@ import {EditEndingMatchComponent} from 'src/app/edit-match/edit-ending-match.com
   template: `
 
 
-    <ion-header>
-      <ion-toolbar>
-        <ion-title>Player Position</ion-title>
-      </ion-toolbar>
-    </ion-header>
-    <ion-item>
-      <ion-button (click)="registerPlayerPosition(selectedArea)" [disabled]="!selectedArea" color="primary">Next Point
-      </ion-button>
-      <ion-button (click)="endMatchModal(selectedArea)" [disabled]="!selectedArea" color="danger">End Match</ion-button>
+      <ion-header>
+          <ion-toolbar>
+              <ion-title>Player Position</ion-title>
+          </ion-toolbar>
+      </ion-header>
+      <ion-item>
+          <ion-button (click)="registerPlayerPosition(selectedArea)" [disabled]="!selectedArea" color="primary">Next Point
+          </ion-button>
+          <ion-button (click)="endMatchModal(selectedArea)" [disabled]="!selectedArea" color="danger">End Match
+          </ion-button>
+      </ion-item>
 
-    </ion-item>
-
-    <ion-content>
-
-
-      <img src="assets/img/tennis_court_color.png" alt="Tennis Court"
-           style="width: auto; height: auto; position: relative;">
-      <ion-button
-        [class.selected]="selectedArea === 'Green'"
-        (click)="selectCourtArea('Green')"
-        [style.background-color]="selectedArea === 'Green' ? 'rgba(0, 255, 0, 0.3)' : 'transparent'"
-        style="position: absolute; left: 1%; top: 3%; width: 31%; height: 18%;   border: 2px solid rgba(0, 255, 0, 1); border-radius: 10px;cursor: pointer;">
-      </ion-button>
-      <ion-button
-        [class.selected]="selectedArea === 'Yellow'"
-        (click)="selectCourtArea('Yellow')"
-        [style.background-color]="selectedArea === 'Yellow' ? 'rgba(255, 255, 0, 0.3)' : 'transparent'"
-        style="position: absolute; left: 1%; top: 22%; width: 31%; height: 50%;  border: 2px solid rgba(255, 255, 0, 1); border-radius: 10px; cursor: pointer;">
-
-      </ion-button>
-
-
-      <ion-button
-        [class.selected]="selectedArea === 'Red'"
-        (click)="selectCourtArea('Red')"
-        [style.background-color]="selectedArea === 'Red' ? 'rgba(255, 0, 0, 0.3)' : 'transparent'"
-        style="position: absolute; left: 1%; top: 73%; width: 31%; height: 22%; border: 2px solid rgba(255, 0, 0, 1); border-radius: 10px; cursor: pointer;">
-      </ion-button>
-
-
-    </ion-content>
+      <ion-content>
+          <div class="court">
+              <svg [class.selected]="selectedArea === 'Green'"
+                   (click)="selectCourtArea('Green')"
+                   [style.border]="selectedArea === 'Green' ? '10px solid green' : 'transparent'" width="20vw"
+                   viewBox="0 0 855 233" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"
+                   style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect x="0" y="0" width="854.627" height="1043.69" style="fill:#66d47d;"/>
+                  <g><path d="M117.5,2.92l-0,1037.62" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M747.497,3.045l0,1037.31" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M424.646,3.092l0,612.361" style="fill:none;stroke:#fff;stroke-width:6.25px;"/></g>
+                  <rect x="0" y="0" width="854.627" height="232.409" style="fill:none;"/>
+                  <rect x="0" y="2.92" width="854.627" height="229.489" style="fill:none;"/></svg>
+            <br>
+              <svg [class.selected]="selectedArea === 'Yellow'"
+                   (click)="selectCourtArea('Yellow')"
+                   [style.border]="selectedArea === 'Yellow' ? '10px solid yellow' : 'transparent'" width="20vw"
+                   viewBox="0 0 855 613" version="1.1" xmlns="http://www.w3.org/2000/svg"
+                   xml:space="preserve"
+                   style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect x="0" y="-232.409" width="854.627" height="1043.69" style="fill:#ebdc55;"/>
+                  <g><path d="M117.5,-229.489l-0,1037.62" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M117.5,383.77l629.793,-0" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M747.497,-229.365l0,1037.31" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M424.646,-229.317l0,612.361" style="fill:none;stroke:#fff;stroke-width:6.25px;"/></g>
+                  <rect x="-0" y="-0" width="854.627" height="612.487" style="fill:none;"/>
+                  <rect x="0" y="-229.489" width="854.627" height="229.489" style="fill:none;"/></svg>
+            <br>
+              <svg [class.selected]="selectedArea === 'Red'"
+                   (click)="selectCourtArea('Red')"
+                   [style.border]="selectedArea === 'Red' ? '10px solid red' : 'transparent'" width="20vw"
+                   viewBox="0 0 855 199" version="1.1" xmlns="http://www.w3.org/2000/svg" xml:space="preserve"
+                   style="fill-rule:evenodd;clip-rule:evenodd;stroke-linecap:square;stroke-linejoin:round;stroke-miterlimit:1.5;"><rect x="0" y="-844.897" width="854.627" height="1043.69" style="fill:#d84435;"/>
+                  <g><path d="M117.5,-841.976l-0,1037.62" style="fill:none;stroke:#fff;stroke-width:6.25px;"/>
+                      <path d="M747.497,-841.852l0,1037.31" style="fill:none;stroke:#fff;stroke-width:6.25px;"/></g>
+                  <rect x="0" y="0" width="854.627" height="198.792" style="fill:none;"/></svg>
+          </div>
+      </ion-content>
   `,
   styleUrls: ['./player-position.component.scss'],
 })
