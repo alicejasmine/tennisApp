9d7bfacd4884e9a73e25a868eac38f27a3fc9608
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-12-16 14:30:13 +0100

    refactoring to use editMatch method from service

--- frontend/src/app/edit-match/edit-ending-match.component.ts
+++ frontend/src/app/edit-match/edit-ending-match.component.ts
@@ -1,5 +1,5 @@
 import {Component} from "@angular/core";
-import {FormBuilder, Validators} from "@angular/forms";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {ModalController, ToastController} from "@ionic/angular";
 import {HttpClient, HttpErrorResponse} from "@angular/common/http";
 import {DataService} from "../data.service";
@@ -7,6 +7,7 @@ import {Router} from "@angular/router";
 import {firstValueFrom} from "rxjs";
 import {MatchWithPlayers} from "../models";
 import {ShotService} from "src/services/shot.service";
+import { MatchService } from "src/services/match.service";
 
 @Component({
     template: `
@@ -37,7 +38,7 @@ import {ShotService} from "src/services/shot.service";
 
 export class EditEndingMatchComponent {
 
-    constructor(public fb: FormBuilder, public modalController: ModalController, public http: HttpClient, public dataService: DataService, public toastController: ToastController, public router: Router, public shotService: ShotService) {
+    constructor(public fb: FormBuilder, public modalController: ModalController, public http: HttpClient, public dataService: DataService, public toastController: ToastController, public router: Router, public shotService: ShotService, public matchService:MatchService) {
 
     }
 
@@ -56,44 +57,9 @@ export class EditEndingMatchComponent {
     });
 
 
-    async editMatch() {
-        try {
-            const call = this.http.put<MatchWithPlayers>('api/matches/' + this.dataService.currentMatch.id, this.editMatchForm.value);
-            const result = await firstValueFrom<MatchWithPlayers>(call);
-            let index = this.dataService.matchesWithPlayers.findIndex(m => m.id == this.dataService.currentMatch.id)
-            this.dataService.matchesWithPlayers[index] = result;
-            this.dataService.currentMatch = result;
-            this.modalController.dismiss();
-            const toast = await this.toastController.create({
-                message: 'Match updated',
-                duration: 1000,
-                color: 'success'
-            })
-            toast.present();
-        } catch (error: any) {
-            console.log(error);
-            let errorMessage = 'Error';
 
-            if (error instanceof HttpErrorResponse) {
-                // The backend returned an unsuccessful response code.
-                errorMessage = error.error?.message || 'Server error';
-            } else if (error.error instanceof ErrorEvent) {
-                // A client-side or network error occurred.
-                errorMessage = error.error.message;
-            }
-
-            const toast = await this.toastController.create({
-                color: 'danger',
-                duration: 2000,
-                message: errorMessage
-            });
-
-            toast.present();
-        }
-    }
-
-    async endMatch() {
-        this.editMatch();
+     endMatch() {
+        this.matchService.editMatch(this.editMatchForm);
         this.shotService.registerShot();
         this.router.navigate(['/match-info/' + this.dataService.currentMatch.id]);
 
