c0476617b22088a2622284db474866e19b463966
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-12-16 11:11:38 +0100

    end match test

--- tests/ShotTests/ShotTestsUI.cs
+++ tests/ShotTests/ShotTestsUI.cs
@@ -1,3 +1,5 @@
+using System.Text.RegularExpressions;
+using Bogus.DataSets;
 using Dapper;
 using FluentAssertions;
 using Microsoft.Playwright;
@@ -8,7 +10,7 @@ namespace tests;
 
 public class ShotTestsUI : PageTest
 {
-    [TestCase("Forced Error", "Backhand Return", "Long", "Down The Line", "Yellow")]
+ 
     [TestCase("Unforced Error", "Overhead", "Net", "Cross Court", "Green")]
     [TestCase("Winner", "Forehand Groundstroke", "Not Applicable", "Middle", "Red")]
     public async Task ShotCanSuccessfullyBeRegistered(string shotClassification, string shotType,
@@ -23,7 +25,7 @@ public class ShotTestsUI : PageTest
         await using (var conn = await Helper.DataSource.OpenConnectionAsync())
         {
             conn.Query(
-                "INSERT INTO tennis_app.match (environment, surface, date, start_time, end_time, finished, notes) VALUES ('hardcodedOutdoor', 'hardcodedClay', '2001-01-01', '2001-01-02 10:10:10', '2001-01-03 11:11:11', false, 'hardcodedNote') RETURNING *;" +
+                "INSERT INTO tennis_app.match (environment, surface, date, start_time, end_time, finished, notes) VALUES ('outdoor', 'clay', '2001-01-01', '2001-01-02 10:10:10', '2001-01-03 11:11:11', false, 'hardcodedNote') RETURNING *;" +
                 "insert into tennis_app.players(full_name)VALUES('Mette Jensen');" +
                 "insert into tennis_app.players(full_name)VALUES('Emilie Larsen');" +
                 " INSERT INTO tennis_app.played_in (player_id, match_id) VALUES (1,1);" +
@@ -82,8 +84,10 @@ public class ShotTestsUI : PageTest
                 .BeEquivalentTo(expectedShot); //Should be equal to shot found in DB
         }
     }
-
-    public async Task MatchCanSuccessfullyEndUI()
+    [Test]
+    [TestCase("Winner", "Forehand Groundstroke", "Not Applicable", "Middle", "Red")]
+    public async Task MatchCanSuccessfullyEndUI(string shotClassification, string shotType,
+        string shotDestination, string shotDirection, string playerPosition)
     {
         //ARRANGE
         Helper.TriggerRebuild();
@@ -93,14 +97,65 @@ public class ShotTestsUI : PageTest
         await using (var conn = await Helper.DataSource.OpenConnectionAsync())
         {
             conn.Query(
-                "INSERT INTO tennis_app.match (environment, surface, date, start_time, end_time, finished, notes) VALUES ('hardcodedOutdoor', 'hardcodedClay', '2001-01-01', '2001-01-02 10:10:10', '2001-01-03 11:11:11', false, 'hardcodedNote') RETURNING match_id as Id, environment as Environment, surface as Surface, date as Date, start_time as StartTime, end_time as EndTime, finished as Finished, notes as Notes;" +
+                "INSERT INTO tennis_app.match (environment, surface, date, start_time, end_time, finished, notes) VALUES ('outdoor', 'clay', '2024-01-01', '2024-01-02 10:10:10', '2024-01-03 11:11:11', false, 'hardcodedNote') RETURNING match_id as Id, environment as Environment, surface as Surface, date as Date, start_time as StartTime, end_time as EndTime, finished as Finished, notes as Notes;" +
                 "insert into tennis_app.players(full_name)VALUES('Mette Jensen');" +
                 "insert into tennis_app.players(full_name)VALUES('Emilie Larsen');" +
                 " INSERT INTO tennis_app.played_in (player_id, match_id) VALUES (1,1);" +
                 "INSERT INTO tennis_app.played_in (player_id, match_id) VALUES (2,1)" +
                 "");
         }
-       
+
+        var updatedNotes = "updated notes";
         
+        //ACT
+        //register shot
+        
+        await Page.GotoAsync("http://localhost:4200/match-info/1");
+
+        await Page.GetByRole(AriaRole.Link, new() { Name = "Track shots" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Start Match" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = shotClassification }).First.ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Next" }).ClickAsync();
+
+        await Task.Delay(2000);
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = shotType }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Next" }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = shotDestination }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = shotDirection, }).ClickAsync();
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "Next" }).ClickAsync();
+
+        await Page.GetByTestId(playerPosition).ClickAsync();
+        //end match
+        
+        await Page.GetByRole(AriaRole.Button, new() { Name = "End Match" }).ClickAsync();
+
+        await Page.GetByLabel("NoteshardcodedNote").ClickAsync();
+
+        await Page.GetByLabel("NoteshardcodedNote").FillAsync(updatedNotes);
+
+        await Page.GetByRole(AriaRole.Button, new() { Name = "End Match" }).ClickAsync();
+        
+        
+        
+        //ASSERT
+        await using (var conn = await Helper.DataSource.OpenConnectionAsync())
+        {
+            var matchResult = conn.QueryFirst<Match>(
+                "SELECT m.match_id as Id, m.environment as Environment, m.surface as Surface, m.date as Date, m.start_time as StartTime, m.end_time as EndTime, m.finished as Finished, m.notes as Notes" +
+                " FROM tennis_app.match m ");
+            matchResult.Finished.Should().Be(true); //match status has changed to finished
+            matchResult.Notes.Should().Be(updatedNotes);
+            
+        }
+     
+    
     }
 }
