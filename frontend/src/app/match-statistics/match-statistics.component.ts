a5cda387981000dbaaa3f91f953884080cc9377c
alicejasmine <82236372+alicejasmine@users.noreply.github.com>
2023-12-18 19:04:55 +0100

    added graphs for shot type in match statistics screen

--- frontend/src/app/match-statistics/match-statistics.component.ts
+++ frontend/src/app/match-statistics/match-statistics.component.ts
@@ -9,6 +9,8 @@ import {ModalController} from "@ionic/angular";
 import {ChartOptions} from "chart.js";
 import {ShotService} from "src/services/shot.service";
 
+
+
 @Component({
   selector: 'app-match-statistics',
   templateUrl: './match-statistics.component.html',
@@ -20,16 +22,43 @@ export class MatchStatisticsComponent implements OnInit {
 
   public pieChartOptions: ChartOptions<'pie'> = {
     responsive: true,
-  };
-  public pieChartLabels = ['Winner', 'Forced Error', 'Unforced Error'];
-  public pieChartDatasets1 = [{
+  }
+
+  public pieChartLabelClassification = ['Winner', 'Forced Error', 'Unforced Error'];
+  public pieChartLabelType = [
+    'Forehand Groundstroke', 'Forehand Return ',
+    'Serve Deuce ', 'Serve Add',
+    'Backhand Groundstroke', 'Backhand Return ',
+    'Forehand Volley', 'Backhand Volley ',
+    'Overhead ', 'Other'
+  ];
+  public pieChartDataset1 = [{
+    data: [0, 0, 0],
+  }];
+  public pieChartDataset2 = [{
     data: [0, 0, 0]
   }];
-  public pieChartDatasets2 = [{
-    data: [0, 0, 0]
+  public pieChartDatasetWinner1 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }];
+  public pieChartDatasetWinner2 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }];
+
+  public pieChartDatasetFE1 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }];
+  public pieChartDatasetFE2 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }];
+
+  public pieChartDatasetUE1 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+  }];
+  public pieChartDatasetUE2 = [{
+    data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
   }];
   public pieChartLegend = true;
-  public pieChartPlugins = [];
 
   constructor(public http: HttpClient, public dataService: DataService, public route: ActivatedRoute, public router: Router, public modalController: ModalController, public shotService: ShotService) {
 
@@ -76,21 +105,27 @@ export class MatchStatisticsComponent implements OnInit {
   }
 
   async loadGraphs() {
-
-    if (this.dataService.currentMatch.playerId1 && this.dataService.currentMatch.playerId2 && this.dataService.currentMatch.id) {
-      await this.shotService.countShotsForPlayerByMatch(this.dataService.currentMatch.playerId1, this.dataService.currentMatch.id);
-      await this.shotService.countShotsForPlayerByMatch(this.dataService.currentMatch.playerId2, this.dataService.currentMatch.id);
-
-      this.pieChartDatasets1 = [{
+    if (this.dataService.currentMatch.finished) {
+      if (this.dataService.currentMatch.playerId1 && this.dataService.currentMatch.playerId2 && this.dataService.currentMatch.id) {
+        await this.shotService.countShotsForPlayerByMatch(this.dataService.currentMatch.playerId1, this.dataService.currentMatch.id);
+        await this.shotService.countShotsForPlayerByMatch(this.dataService.currentMatch.playerId2, this.dataService.currentMatch.id);
+      }
+      this.pieChartDataset1 = [{
         data: [this.shotService.winnerCountPlayer1, this.shotService.forcedErrorCountPlayer1, this.shotService.unforcedErrorCountPlayer1]
       }];
 
-      this.pieChartDatasets2 = [{
+      this.pieChartDataset2 = [{
         data: [this.shotService.winnerCountPlayer2, this.shotService.forcedErrorCountPlayer2, this.shotService.unforcedErrorCountPlayer2]
       }];
-    }
 
+      this.winnerGraphs();
+      this.feGraphs();
+      this.ueGraphs();
+
+
+    }
   }
+
 
   async loadData() {
 
@@ -100,4 +135,100 @@ export class MatchStatisticsComponent implements OnInit {
     }
   }
 
+
+  winnerGraphs() {
+    this.pieChartDatasetWinner1 = [{
+      data: [
+        this.shotService.forehandGroundstrokeWinnerCountPlayer1,
+        this.shotService.forehandReturnWinnerCountPlayer1,
+        this.shotService.serveDeuceWinnerCountPlayer1,
+        this.shotService.serveAddWinnerCountPlayer1,
+        this.shotService.backhandGroundstrokeWinnerCountPlayer1,
+        this.shotService.backhandReturnWinnerCountPlayer1,
+        this.shotService.forehandVolleyWinnerCountPlayer1,
+        this.shotService.backhandVolleyWinnerCountPlayer1,
+        this.shotService.overheadWinnerCountPlayer1,
+        this.shotService.otherWinnerCountPlayer1,
+      ]
+    }];
+
+    this.pieChartDatasetWinner2 = [{
+      data: [
+        this.shotService.forehandGroundstrokeWinnerCountPlayer2,
+        this.shotService.forehandReturnWinnerCountPlayer2,
+        this.shotService.serveDeuceWinnerCountPlayer2,
+        this.shotService.serveAddWinnerCountPlayer2,
+        this.shotService.backhandGroundstrokeWinnerCountPlayer2,
+        this.shotService.backhandReturnWinnerCountPlayer2,
+        this.shotService.forehandVolleyWinnerCountPlayer2,
+        this.shotService.backhandVolleyWinnerCountPlayer2,
+        this.shotService.overheadWinnerCountPlayer2,
+        this.shotService.otherWinnerCountPlayer2,
+      ]
+    }];
+  }
+
+  feGraphs() {
+    this.pieChartDatasetFE1 = [{
+      data: [
+        this.shotService.forehandGroundstrokeFECountPlayer1,
+        this.shotService.forehandReturnFECountPlayer1,
+        this.shotService.serveDeuceFECountPlayer1,
+        this.shotService.serveAddFECountPlayer1,
+        this.shotService.backhandGroundstrokeFECountPlayer1,
+        this.shotService.backhandReturnFECountPlayer1,
+        this.shotService.forehandVolleyFECountPlayer1,
+        this.shotService.backhandVolleyFECountPlayer1,
+        this.shotService.overheadFECountPlayer1,
+        this.shotService.otherFECountPlayer1,
+      ]
+    }];
+
+    this.pieChartDatasetFE2 = [{
+      data: [
+        this.shotService.forehandGroundstrokeFECountPlayer2,
+        this.shotService.forehandReturnFECountPlayer2,
+        this.shotService.serveDeuceFECountPlayer2,
+        this.shotService.serveAddFECountPlayer2,
+        this.shotService.backhandGroundstrokeFECountPlayer2,
+        this.shotService.backhandReturnFECountPlayer2,
+        this.shotService.forehandVolleyFECountPlayer2,
+        this.shotService.backhandVolleyFECountPlayer2,
+        this.shotService.overheadFECountPlayer2,
+        this.shotService.otherFECountPlayer2,
+      ]
+    }];
+  }
+
+  ueGraphs() {
+    this.pieChartDatasetUE1 = [{
+      data: [
+        this.shotService.forehandGroundstrokeUECountPlayer1,
+        this.shotService.forehandReturnUECountPlayer1,
+        this.shotService.serveDeuceUECountPlayer1,
+        this.shotService.serveAddUECountPlayer1,
+        this.shotService.backhandGroundstrokeUECountPlayer1,
+        this.shotService.backhandReturnUECountPlayer1,
+        this.shotService.forehandVolleyUECountPlayer1,
+        this.shotService.backhandVolleyUECountPlayer1,
+        this.shotService.overheadUECountPlayer1,
+        this.shotService.otherUECountPlayer1,
+      ]
+    }];
+
+    this.pieChartDatasetUE2 = [{
+      data: [
+        this.shotService.forehandGroundstrokeUECountPlayer2,
+        this.shotService.forehandReturnUECountPlayer2,
+        this.shotService.serveDeuceUECountPlayer2,
+        this.shotService.serveAddUECountPlayer2,
+        this.shotService.backhandGroundstrokeUECountPlayer2,
+        this.shotService.backhandReturnUECountPlayer2,
+        this.shotService.forehandVolleyUECountPlayer2,
+        this.shotService.backhandVolleyUECountPlayer2,
+        this.shotService.overheadUECountPlayer2,
+        this.shotService.otherUECountPlayer2,
+      ]
+    }];
+  }
 }
